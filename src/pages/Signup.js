148b758820092b2476e782e41fef680f34d0351b
Rahul <chrahulofficial@gmail.com>
2024-07-13 21:35:11 +0530

    done

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import google from "../images/google.png";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../Firebase";
@@ -6,13 +6,26 @@ import { auth } from "../Firebase";
 export default function Signup() {
   const provider = new GoogleAuthProvider();
   // const navigate = useNavigate();
-  const GoogleSignIn = async () => {
+
+  const [data, setdata] = useState({
+    email: "",
+    password: "",
+  });
+
+  const googleSignIn = async () => {
     try {
       const res = await signInWithPopup(auth, provider);
       // saving in db
       console.log(res.user);
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  const emailSignUp = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -28,6 +41,10 @@ export default function Signup() {
             <input
               type="text"
               placeholder="Email"
+              value={data.email}
+              onChange={(e) => {
+                setdata({ ...data, email: e.target.value });
+              }}
               className="bg-[#1a292e] p-3  rounded-lg text-white outline-none w-[75vw] lg:w-[30vw]"
             />
           </div>
@@ -35,6 +52,10 @@ export default function Signup() {
             <h1 className="font-semibold text-slate-300">Password</h1>
             <input
               type="password"
+              value={data.password}
+              onChange={(e) => {
+                setdata({ ...data, password: e.target.value });
+              }}
               placeholder="Password"
               className="bg-[#1a292e] p-3  rounded-lg text-white outline-none w-[75vw] lg:w-[30vw]"
             />
@@ -49,7 +70,7 @@ export default function Signup() {
           </div>
           <div className="flex justify-center">
             <button
-              onClick={GoogleSignIn}
+              onClick={googleSignIn}
               className="bg-[#00e1db] px-10 py-2 rounded-lg w-[75vw] lg:w-[30vw] flex items-center space-x-4 justify-center"
             >
               <img src={google} className="w-8 h-8" alt="" />
