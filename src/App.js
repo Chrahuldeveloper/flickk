0797689ccb1cd21b60abd70518f98dbd470b9252
Rahul <chrahulofficial@gmail.com>
2024-08-02 14:48:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import SignUp from "./Pages/SignUp";
+import { Home, SignUp } from "./Pages";
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
